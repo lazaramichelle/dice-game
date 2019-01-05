39e0fcf144ce4d4a6e66aa6164c6cad0e26dd605
lmichelletech <lmichelle@techlaunch.io>
2019-01-04 22:16:43 -0500

    completed game, added rules, made it two dice instead of one, and made minor design changes

--- app.js
+++ app.js
@@ -1,10 +1,162 @@
-/*
-GAME RULES:
+var scores, roundScore, activePlayer, gamePlaying;
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+init();
 
-*/
+var lastDice;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1. Random number
+        var count = 0;
+        var dice1 = 0;
+        var dice2 = 0;
+        var roll = [];
+
+        // 2. roll dice and Display the result
+        rollDice(count, thingsToDoAfter);
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        function thingsToDoAfter(roll){
+            // console.log('test >>> ' + roll);
+            dice1=roll[0];
+            dice2=roll[1];
+            
+            if (dice1 !== 1 && dice2 !== 1) {
+                //Add score
+                roundScore += dice1 + dice2;
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            } else {
+                //Next player
+                document.getElementById('end-turn').style.display = 'block';
+                nextPlayer();
+            }
+        }
+    }    
+});
+
+document.querySelector('.btn-collect').addEventListener('click', function() {
+    checkForWin();
+});
+
+function checkForWin(){
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        // Undefined, 0, null or "" are COERCED to false
+        // Anything else is COERCED to true
+        if(input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+        
+        // Check if player won the game
+        if (scores[activePlayer] >= winningScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }
+}
+
+function rollDice(count, callback){
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    var result = [dice1, dice2];
+    document.getElementById('end-turn').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+    // console.log("Count: " + count+ " dice1: " + dice1 + " dice2: " + dice2 + " result>>>> " + result[0] + " " + result[1]);
+   
+    if(count < 4){
+        setTimeout(function(){  
+            count++;          
+            rollDice(count, callback);
+        }, 200);
+    }
+
+    if (count === 4){
+        // console.log("Count>>>>>> " + count + " " + dice1 + " " + dice2 + " " + result[0] + " " + result[1]);
+        callback(result);
+    }        
+}
+
+function nextPlayer() {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    document.getElementById('end-turn').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+// Get the modal
+var modal = document.getElementById('rulesModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("rulesBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
